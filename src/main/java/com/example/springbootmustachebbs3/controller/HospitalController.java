b90d9284dce4dace950f794a7a4976aa2d797b91
chlalswns200 <chlalswns200@naver.com>
2022-11-11 13:20:16 +0900

    페이징 전까지 완료

--- src/main/java/com/example/springbootmustachebbs3/controller/HospitalController.java
+++ src/main/java/com/example/springbootmustachebbs3/controller/HospitalController.java
@@ -3,6 +3,10 @@ package com.example.springbootmustachebbs3.controller;
 import com.example.springbootmustachebbs3.domain.Hospital;
 import com.example.springbootmustachebbs3.repository.HospitalRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +26,11 @@ public class HospitalController {
         this.hospitalRepository = hospitalRepository;
     }
 
+    @GetMapping("")
+    public String index() {
+        return "redirect:/hospital/list";
+    }
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Hospital> all = hospitalRepository.findAll();
@@ -33,6 +42,17 @@ public class HospitalController {
             if(i==10) break;
         }
         model.addAttribute("hospitals",test);
+        return "hospital/list";
+    }
+
+    @GetMapping("/listV2")
+    public String listV2(Model model, @PageableDefault(size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<Hospital> all = hospitalRepository.findAll(pageable);
+
+        model.addAttribute("hospitals",all);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
         return "hospital/list";
     }
 
